18a897b737cd024a9d9c9ea92c3f1f3f7ded0f0c
athallahra123 <102503406+athallahra123@users.noreply.github.com>
2022-07-11 18:34:14 +0700

    Add files via upload

---
+++ locales/id.lua
@@ -0,0 +1,9 @@
+Locales['id'] = {
+  ['valid_purchase'] = 'konfirmasi pembelian ini?',
+  ['yes'] = 'ya',
+  ['no'] = 'tidak',
+  ['not_enough_money'] = 'kamu tidak memiliki cukup uang',
+  ['press_access'] = 'tekan ~INPUT_CONTEXT~ untuk mengakses ~y~Toko Barber~s~.',
+  ['barber_blip'] = 'toko Barber',
+  ['you_paid'] = 'kamu membayar ~g~$%s~s~',
+}
